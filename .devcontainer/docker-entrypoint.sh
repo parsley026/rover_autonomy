04900c712e01f5c7c5e4c082b8b9d01e46bb1535
parsley026 <245828@edu.p.lodz.pl>
2026-09-02 01:21:46 +0200

    feat: updated docker structure

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -31,6 +31,14 @@ echo "--------------------------------------------------------------------"
 
 rm -f /tmp/rexlaunch.pgid # remove old PGID file
 
+# Copy host authorized keys for use with user rex
+if [ -s /run/secrets/authorized_keys ]; then
+    install -o rex -g "$(id -g rex)" -m 0600 /run/secrets/authorized_keys /home/rex/.ssh/authorized_keys \
+    || echo "Couldn't copy authorized_keys file"
+else
+    echo "No authorized keys copied into the container"
+fi
+
 if service ssh start; then
     # Get the SSH port from the configuration file
     SSH_PORT_CONFIGURED=$(grep -oP '^Port\s+\K\d+' /etc/ssh/sshd_config)
@@ -43,6 +51,14 @@ if service ssh start; then
     fi
 else
     echo "SSH service failed to start"
+fi
+
+echo "--------------------------------------------------------------------"
+
+if service xrdp start; then
+    echo "XRDP service started successfully."
+else
+    echo "XRDP service failed to start"
 fi
 
 echo "--------------------------------------------------------------------"
